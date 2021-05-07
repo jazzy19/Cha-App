8185842bce28b630b183db2799e676ab1b86a22c
JAY SOLANKI <jaysolanki736@gmail.com>
2021-05-07 18:22:25 +0530

    Minor

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const socket = require("socket.io");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 
 app.use(express.static("public"));
 
-const PORT = 4005;
-const server = app.listen(process.env.PORT, "0.0.0.0", () => {
-  console.log(`Server Started at ${process.env.PORT} `);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Expose-Headers", "isenc");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, identity, authorization, isenc"
+  );
+
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  if (req.method === "OPTIONS") {
+    res.statusCode = 200;
+    res.end();
+  }
+  next();
+});
+
+const PORT = process.env.PORT || 4005;
+const server = app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server Started at ${PORT} `);
 });
 
 const io = socket(server);
